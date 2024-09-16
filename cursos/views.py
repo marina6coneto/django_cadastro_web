d37286e3d3991c907bf00de85699698608fca619
marina6coneto <santoscmarina@hotmail.com>
2024-09-16 16:51:49 -0300

    cursos folder

---
+++ cursos/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from cursos.forms import CursoForm
+from cursos.models import Curso
+from django.views.decorators.cache import cache_page
+
+# Create your views here.
+@cache_page(30)
+def criar_curso(request):
+    cursos = Curso.objects.all()
+    form = CursoForm(request.POST or None)
+    sucesso = False
+    
+    if form.is_valid():
+        form.save()
+        sucesso = True
+        
+    contexto = {
+        'form': form,
+        'sucesso': sucesso,
+        'cursos': cursos,
+    }
+    
+    return render(request, 'criar_curso.html', contexto)
