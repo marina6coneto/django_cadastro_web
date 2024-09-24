193bafae4eb9f3a451dd0f6ae422e52aa5f7d9a7
marina6coneto <santoscmarina@hotmail.com>
2024-09-24 20:20:57 -0300

    add rest api

---
+++ rest_api/views.py
@@ -0,0 +1,17 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from cursos.models import Curso
+from rest_api.serializers import CursoModelSerializer
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def hello_world(request):
+    if request.method == 'POST':
+        return Response({'message': f'Hello, {request.data.get("name")}!'})
+    return Response({'message': 'Hello, World!'})
+   
+class CursoModelViewSet(ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoModelSerializer
+    
