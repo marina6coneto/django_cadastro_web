ed2a7156ca0855bc8bd4e2ffb555969e4bf777cc
marina6coneto <santoscmarina@hotmail.com>
2024-09-24 20:20:33 -0300

    add rest api

--- projeto_womakers/urls.py
+++ projeto_womakers/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from base.views import inicio, cadastro
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', inicio),
     path('cadastro/', cadastro),
     path('curso/', include('cursos.urls', namespace='cursos')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('rest_api.urls', namespace='api')),
 ]
